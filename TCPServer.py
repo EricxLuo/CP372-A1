fe75de7bf6707cc750e8b56917e9f7846a0e2bce
HL42 <fuquanlin347@gmail.com>
2025-10-01 16:05:46 -0400

    implement cache

--- TCPServer.py
+++ TCPServer.py
@@ -1,6 +1,44 @@
 import socket
+import threading
+import datetime
+
+client_cache = {}
 
 
+def handle_client(client_socket, client_id):
+    connected_time = datetime.datetime.now()
+    client_cache[client_id] = {"NAME": None, "Connect": connected_time, "Disconnect": None}
+
+    try:
+        while True:
+
+            data = client_socket.recv(1024).decode()
+
+            if not data:
+                break
+
+            if client_cache[client_id]["NAME"] is None:
+                client_cache[client_id]["Name"] = data
+                print(f"Client {client_id} registered as {data}")
+                client_socket.send("Name registered".encode())
+
+            elif data.lower() == 'status':
+                cache_info = "\n".join([
+                    f"Client {cid}: Name={info['Name']}, Connect={info['Connect']}, Disconnect={info['Disconnect']}"
+                    for cid, info in client_cache.items()
+                ])
+                client_socket.send(f"Server Cache:\n{cache_info}".encode())
+
+            else:
+                print(f"Received: {data}")
+                upcased_data = data.upper()
+                client_socket.send(upcased_data.encode())
+
+               
+
+    finally:
+        client_cache[client_id]["Disconnect"] = datetime.datetime.now()
+        client_socket.close()
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,21 +47,18 @@ def start_server():
     print("Server is listening...")
     ClientName = 0
     while True:
-        ClientName +=1
-
+        ClientName += 1
         if ClientName <= 3:
             
-            client_socket, addr = server_socket.accept()
-            print(f"Client{ClientName:02d} Connection from {addr}")
+            client_socket, client_id = server_socket.accept()
+            print(f"Client{ClientName:02d} Connection from {client_id}")
 
-            data = client_socket.recv(1024).decode()
-            if data:
-                print(f"Received: {data}")
-                upcased_data = data.upper()
-                client_socket.send(upcased_data.encode())
+            # Corrected thread creation
+            client_handler = threading.Thread(target=handle_client, args=(client_socket, client_id))
+            client_handler.start()
 
-            client_socket.close()
-        client_socket.close()
+        else:
+            break 
 
 if __name__ == '__main__':
     start_server()
--- TCPclient.py
+++ TCPclient.py
@@ -4,11 +4,21 @@ def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 12345))  # Connect to the server
 
-    message = input("Enter message to send: ")
+    # Send initial name to the server
+    message = input("Enter your name to the server: ")
     client_socket.send(message.encode())
 
-    data = client_socket.recv(1024).decode()
-    print(f"Received from server: {data}")
+    while True:
+        message = input("Enter a message or type 'status' to see the server cache (type 'exit' to quit): ")
+
+        if message.lower() == 'exit':
+            break  # Exit the loop and close the connection
+
+        client_socket.send(message.encode())  # Send the message to the server
+
+        # Receive and print response from the server
+        data = client_socket.recv(1024).decode()
+        print(f"Received from server: {data}")
 
     client_socket.close()
 
